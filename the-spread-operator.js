5625ddb00f131029eca13fd17e0cc83a021e60b0
CarlG96 <carl.gardiner1@hotmail.com>
2022-07-12 11:50:55 +0000

    commit

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,6 +4,11 @@
  */
 
 // No spread operator
+let arr1 = [1,2,3];
+let arr2 = arr1;
+arr2.push(4);
+console.log(arr2); //displays 1,2,3,4
+console.log(arr1); //also displays 1,2,3,4 because arr2 points to arr1
 
 // Copying an array
 
